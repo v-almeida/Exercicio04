4b8e3d094c2dc1adc588fffaa8a5849f30fe43e6
Vítor <almeida.vitor20000@gmail.com>
2024-05-06 21:33:36 -0300

    cenario06 no padrão MVC

---
+++ cenario06/src/br/edu/up/models/Aeroporto.java
@@ -0,0 +1,17 @@
+/*Um aeroporto precisa-se registrar informações sobre as diferentes pessoas que nele trafegam.
+Todas as pessoas possuem nome e rg. As pessoas se dividem entre passageiros e tripulação. Os
+passageiros possuem um identificador de bagagem e a sua passagem. Sobre a passagem, armazena-se
+o número do acento, a classe do acento e a data do voo, contendo dia, mês, ano, hora e minuto de
+partida. Sobre a tripulação, sabe-se a sua identificação aeronáutica e matrícula do funcionário. Dos
+comandantes, registra-se o seu total de horas de voo e dos comissários os idiomas em que possuem
+fluência. Todas as pessoas possuem ainda informações sobre a aeronave em que farão o voo. Sobre
+ela, armazena-se o seu código, tipo e quantidade de assentos. O sistema deve ser desenvolvido no
+padrão arquitetural Model-View-Controller (MVC), possuir uma classe Programa que inicia a execução
+do software e um menu que permita ao usuário realizar as operações necessários para o controle do
+tráfego */
+
+package br.edu.up.models;
+
+public class Aeroporto {
+
+}
