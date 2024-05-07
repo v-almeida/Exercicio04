e089fde54f579431882e4cc4722e2f8cd0d0cd6d
Vítor <almeida.vitor20000@gmail.com>
2024-05-06 21:05:14 -0300

    adição do cenario04 nos modelos de MVC

---
+++ cenario04/src/br/edu/up/models/Estacionamento.java
@@ -0,0 +1,16 @@
+/*Um estacionamento precisa controlar o fluxo de veículos que entram e saem diariamente de seu
+espaço. Para isso, seu operador precisa de um sistema que registre as informações de modelo, placa e
+cor dos carros toda vez que eles chegam. Ao entrar no estacionamento o carro ocupa uma das 10 vagas
+disponíveis, portanto é necessário verificar se o estacionamento ainda dispõe de vagas e na retirada é
+necessário buscar o veículo através de sua placa. No final de cada período, manhã, tarde ou noite, é
+necessário emitir um relatório que informe quantos veículos entraram e saíram do estacionamento e o
+valor de pagamentos naquele período, considerando o valor do por período é de R$ 5,00. O sistema
+deve ser desenvolvido no padrão arquitetural Model-View-Controller (MVC), possuir uma classe
+Programa que inicia a execução do software e um menu que permita ao usuário realizar as operações de
+entrada e saída de veículos. */
+
+package br.edu.up.models;
+
+public class Estacionamento {
+
+}
