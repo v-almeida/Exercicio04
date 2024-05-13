8d3bc940867d0dafca443bcde8782789f5e97e6f
Vítor <almeida.vitor20000@gmail.com>
2024-05-13 20:36:20 -0300

    instanciando o cenario04

--- cenario03/src/br/edu/up/Ano.java
+++ cenario03/src/br/edu/up/Ano.java
@@ -1,39 +1,6 @@
 package br.edu.up;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Ano {
     private int ano;
-    private boolean bissexto;
-    private Map<String, Mes> meses = new HashMap<>();
 
-    public Ano(int ano, boolean bissexto) {
-        this.ano = ano;
-        this.bissexto = bissexto;
-    }
-
-    public void adicionarMes(Mes mes) {
-        meses.put(mes.getNome(), mes);
-    }
-
-    public void excluirCompromisso(String nomeMes, int diaMes, int hora) {
-        Mes mes = meses.get(nomeMes);
-        if (mes != null) {
-            mes.excluirCompromisso(diaMes, hora);
-        }
-    }
-
-    public String listarCompromissos(String nomeMes) {
-        Mes mes = meses.get(nomeMes);
-        return (mes != null) ? mes.listarCompromissos() : "Mês não encontrado.";
-    }
-
-    public String listarCompromissos() {
-        StringBuilder sb = new StringBuilder();
-        for (Mes mes : meses.values()) {
-            sb.append(mes.listarCompromissos()).append("\n");
-        }
-        return sb.toString();
-    }
 }
--- cenario03/src/br/edu/up/Mes.java
+++ cenario03/src/br/edu/up/Mes.java
@@ -1,45 +1,12 @@
 package br.edu.up;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Mes {
     private String nome;
     private int qtdeDias;
-    private List<Dia> dias = new ArrayList<>();
 
     public Mes(int qtdeDias, String nome) {
         this.qtdeDias = qtdeDias;
         this.nome = nome;
-    }
-
-    public void adicionarDia(Dia dia) {
-        dias.add(dia);
-    }
-
-    public void adicionarCompromisso(Compromisso comp, int diaMes) {
-        dias.get(diaMes - 1).adicionarCompromisso(comp);
-    }
-
-    public void adicionarCompromisso(String pessoa, String local, String assunto, int hora, int diaMes) {
-        Compromisso comp = new Compromisso(pessoa, local, assunto, hora);
-        dias.get(diaMes - 1).adicionarCompromisso(comp);
-    }
-
-    public void excluirCompromisso(int diaMes, int hora) {
-        dias.get(diaMes - 1).excluirCompromisso(hora);
-    }
-
-    public String listarCompromissos(int diaMes) {
-        return dias.get(diaMes - 1).listarCompromissos();
-    }
-
-    public String listarCompromissos() {
-        StringBuilder sb = new StringBuilder();
-        for (Dia dia : dias) {
-            sb.append(dia.listarCompromissos()).append("\n");
-        }
-        return sb.toString();
     }
 
     public String getNome() {
